efe84c6445f7c283015f57d801ea20d69f646335
Reakain <reakain@users.noreply.github.com>
2026-06-30 16:02:32 -0700

    first bit of things

---
+++ README.md
@@ -0,0 +1,38 @@
+# Twitch Box
+Raspberry Pi controlled overhead camera rig for video streaming.
+
+
+Grabbed starting code wholesale from [here](https://www.hackster.io/tinkernut/raspberry-pi-twitch-o-matic-190a15) as a starting point.
+
+3D printed parts for mounting everything are [here](https://www.thingiverse.com/thing:7376013).
+
+I wanted to be able to easily record/stream when I'm doing electrical or hardware work, so I made a modified version of the work from Ingo, using a different size extrusion and webcam, and also mounted a raspberry pi for easy system running. I also plan to mount a mini monitor in the future so I can see what the camera sees.
+
+This is still a work in progress, so expect updates as I go.
+
+In total, I wanted a "complete" overhead camera system, which included the camera, a light to make things brighter and clearer, both a wide angle and macro lens so I could should close up and bigger areas, a computer to handle streaming/recording the video, and a mini monitor so I could see both what the camera sees, and any chat comments/etc if I'm using it for streaming. All attached to the single overhead extrusion! (So I could just.... move it around my apartment as I worked in various areas)
+
+As part of this, I predominantly tried to use what I had on hand, so these are by no means the "best" options for the task. That said, all the non-3D printed parts are listed here (plus or minus some screws/nuts/wire... but the screws should all be M3s)
+
+##### Frame
+- Standard tripod
+- 20mm T-slots aluminum extrusion (or any ~< 20mm square rod to mount to. This is what I had on hand)
+- M3x8 screw and hex nut for locking each extrusion mount (one for the camera, one to mount the extrusion, one for the pi... probably two for the monitor)
+
+##### Camera Parts
+- Logitech Webcam C270 (It's what I had on hand lol)
+- M3x30 screw and hex nut to mount the webcam 
+- x2 M3x6  screws for the webcam face plate
+- 24 LED Adafruit Neopixel ring (It's what I had on hand for the ring light)
+- I used [this](https://www.amazon.com/dp/B0FZV1QCXN?th=1) phone camera wide angle lens (0.45X Super Wide Angle Lens (140°) with 12.5X Macro Lens)
+
+##### Computer
+- Raspberry pi 3
+- x4 M3 square nuts
+- x4 M3x?? screws
+
+##### Mini Monitor
+- A mini monitor
+- 
+
+Full write up when I finish testing the setup
---
+++ twitch_stream.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3 
+import subprocess
+import picamera
+import time
+import RPi.GPIO as GPIO
+import board
+import neopixel
+
+# Neopixel ring I had on hand, so neopixels setup using: https://learn.adafruit.com/neopixels-on-raspberry-pi/python-usage
+pixels = neopixel.NeoPixel(board.D14, 24)
+# Details for me! Pin numbers from https://pinout.xyz/
+# 5v power pin 4
+# gnd pin 6
+# led pin 8 (GPIO 14)
+# btn pin 10 (GPIO 15)
+
+##################### Configurable Values! ##################### 
+### GPIO Pin Assignments
+# LEDs
+led_pins = [14]
+# Stream Start Button
+btn_pin = 15
+
+### Twitch Streaming Command Values
+# Twitch stream
+TWITCH = "rtmp://live.justin.tv/app/"
+# Twitch stream key
+KEY= "ENTER YOUR PRIVATE KEY HERE"
+
+### Camera Configuration Values
+camera_format = 'h264'
+resolution = (1080, 720)
+framerate = 25
+camera_name = '/dev/video0'
+
+
+##################### Initialization #####################
+### Setup GPIO Controls
+GPIO.setmode(GPIO.BCM)
+# Button GPIO Setup
+GPIO.setup(btn_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# LED GPIO Setup
+for pin in led_pins:
+	GPIO.setup(pin, GPIO.OUT)
+### Twitch Stream Command/Subprocess
+#stream_cmd = 'avconv -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128k -g 50 -strict experimental -f flv ' + TWITCH + KEY 
+stream_cmd = 'avconv -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i ' + camera_name + ' -f ' + camera_format + ' -i - -vcodec copy -acodec aac -ab 128k -g 50 -strict experimental -f flv ' + TWITCH + KEY 
+stream_pipe = subprocess.Popen(stream_cmd, shell=True, stdin=subprocess.PIPE) 
+### Camera connection
+camera = picamera.PiCamera(resolution=resolution, framerate=framerate) 
+
+
+# Function for turning on/off all the lights. 
+# Boolean input, true is on, false is off
+def lightSwitch(turnOn:bool):
+	pixels.fill((0, 255, 0))
+	# for pin in led_pins:
+	# 	GPIO.output(pin, turnOn)
+
+if __name__ == "__main__":
+	#CREATE VARIABLE TO SIGNIFY WHEN STREAMING
+	start_stream = 0
+	try: 
+		# MAKE SURE ALL LED'S ARE OFF
+		lightSwitch(False)
+		while True:
+			#SET VARIABLE FOR BUTTON
+			input_state = GPIO.input(btn_pin)
+			#IF BUTTON IS TRIGGERED AND NOT STREAMING, THEN START STREAM
+			if input_state == False and start_stream == 0:
+				start_stream = 1
+				camera.start_recording(stream_pipe.stdin, format=camera_format, bitrate = 2000000)
+				lightSwitch(True)
+				print('recording started')
+				time.sleep(0.2)
+			#IF BUTTON IS TREGGERED AND STREAMING, CONTINUE STREAM
+			elif input_state == False and start_stream == 1:
+				camera.wait_recording(1)
+			#IF BUTTON IS NOT TRIGGERED AND STREAMING, STOP STREAM
+			elif input_state == True and start_stream ==1:
+				start_stream = 0
+				camera.stop_recording()
+				print("stopped recording")
+				time.sleep(0.2)
+			#IF BUTTON IS NOT TRIGGERED AND NOT STREAMING, TURN LED'S OFF
+			elif input_state == True and start_stream == 0:
+				lightSwitch(False)
+				time.sleep(0.2)
+				print('awaiting input')
+	#ON KEYBOARD INTERRUPT, CLOSE CAMERA, STREAM AND CLEAN UP GPIO
+	except KeyboardInterrupt:
+		camera.close()
+		print("keyboard interrupt")
+	finally:
+		stream_pipe.stdin.close()
+		stream_pipe.wait()
+		GPIO.cleanup()
+		print("Camera safely shut down")
+		print("Good bye") 
