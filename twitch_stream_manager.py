b7c7b42f42c0d1c2f3fb223e9d6dc63dd1c60e06
Reakain <reakain@users.noreply.github.com>
2026-07-20 20:49:20 -0700

    better for testing

--- twitch_stream_manager.py
+++ twitch_stream_manager.py
@@ -3,3 +3,18 @@
 # 2. Connects to chat to display messages during stream
 # 3. Handles turning on/off the ring light and listens for button to start/stop the stream
 
+from twitch_chat_irc import twitch_chat_irc
+
+def update_text_overlay(message):
+    # TODO: update our list of on-screen messages
+    # Then we call the command to remake our overlay text
+    #draw_overlay(message)
+    print(message)
+
+#on exit:
+def on_exit():
+    connection.close()
+    pixels.fill((0, 0, 0))
+
+connection = twitch_chat_irc.TwitchChatIRC()
+connection.listen(channel_id, on_message=update_text_overlay)
