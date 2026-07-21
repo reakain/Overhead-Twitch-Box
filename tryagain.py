ca5f402d791b4867281e0957297c248722c00191
Reakain <reakain@users.noreply.github.com>
2026-07-20 22:10:24 -0700

    reload!

--- tryagain.py
+++ tryagain.py
@@ -145,7 +145,7 @@ v0_1 = ffmpeg.filter([in_scope.video,alpha_v1],'overlay')
 #instead of overlay, we can just draw text with the drawtext command? but unsure about how we update the text.... i guess we can use a textfile instead. oh! we use a pipe in to pipe each frame! then use that as the overlay? (use the numpy processing example, could use pygame or something to make the text frames if we want)
 # v3 = in_cam.video.drawtext(text='twitch chat here', x=width-(width/3), y=0, fix_bounds=True)
 #v01_text = ffmpeg.overlay(v0_1,ffmpeg.input(msg_frame))
-v01_text = ffmpeg.overlay(in_cam.video,ffmpeg.input(msg_frame))
+v01_text = ffmpeg.overlay(in_cam.video,ffmpeg.input(msg_frame, re=True))
 
 #stream = ffmpeg.output(in_audio, v01_text,out_stream)
 stream = ffmpeg.output(in_audio, v01_text,out_stream, format='flv', flvflags='no_duration_filesize',acodec='aac', vcodec='libx264', preset='ultrafast', tune='zerolatency', video_bitrate=4500000, pix_fmt='yuv420p')
